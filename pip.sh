f4dd649ca014213bc85a8afbda89671b0b858a4a
rosetta@snu.ac.kr <heechan@HEECHANs-MacBook-Air.local>
2024-10-24 11:59:16 +0900

    update dependecies

--- pip.sh
+++ pip.sh
@@ -1,4 +1,5 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install transformers
 pip install einops
+pip install rdkit
 # pip install git+https://github.com/samoturk/mol2vec
