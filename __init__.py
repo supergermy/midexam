96ac079972c74a717d597a9191a7e17cee22a134
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:15:05 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .main import *
+
+__version__ = "0.1"
+__author__ = "supergermy"
+
+__all__ = [
+    'get_data',
+    'get_data_frame',
+    'get_data_frame_with_index',
+    'get_data_frame_with_columns',
+    'get_data_frame_with_index_and_columns'
+]
