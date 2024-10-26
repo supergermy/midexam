610e5531f35a1d29267f56e1a9ddef49aadf3380
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:40:02 +0900

    Update __init__.py

--- midexam/__init__.py
+++ midexam/__init__.py
@@ -1,19 +1,15 @@
-from .models.PropertyRegressors import MolecularPropertyRegressor
-from .models.DifferentialAttention import DifferentialAttentionModel
-from .datasets.get_fingerprints import generate_fingerprints
-from .datasets.get_molecular_descriptors import calculate_descriptors
-from .datasets.get_mol2vec import generate_mol2vec
+from .models.PropertyRegressors import PropertyRegressors
+from .datasets.get_fingerprints import get_fingerprints
+from .datasets.get_molecular_descriptors import get_molecular_descriptors
 from .datasets.get_chembert2as import get_chembert_embeddings
 from .datasets.get_molformers import get_molformer_embeddings
 
 __version__ = '0.1'
 
 __all__ = [
-    'MolecularPropertyRegressor',
-    'DifferentialAttentionModel',
-    'generate_fingerprints',
-    'calculate_descriptors',
-    'generate_mol2vec',
+    'PropertyRegressors',
+    'get_fingerprints',
+    'get_molecular_descriptors',
     'get_chembert_embeddings',
     'get_molformer_embeddings',
 ]
