4fb9c339cdc51d7b9257f66b43636b0b93381472
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:24:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="midexam",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['midexam', 'midexam.*']),
 )
