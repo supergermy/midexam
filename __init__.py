12f23cc9dae7df18ef072647d92d2bb2ac815fc3
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:19:56 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,27 +1,40 @@
-from .models.DifferentialAttention import *
-from .models.PropertyRegressors import *
-from .datasets.create_data_loaders import *
-from .datasets.get_chembert2as import *
-from .datasets.get_fingerprints import *
-from .datasets.get_mol2vec import *
-from .datasets.get_molecular_descriptors import *
-from .datasets.get_molformers import *
+# Import core functionality from datasets
+from .datasets.create_data_loaders import (
+    create_classification_data_loader,
+    create_regression_data_loader
+)
+from .datasets.get_fingerprints import generate_fingerprints
+from .datasets.get_molecular_descriptors import calculate_descriptors
+from .datasets.get_mol2vec import generate_mol2vec
+from .datasets.get_chembert2as import get_chembert_embeddings
+from .datasets.get_molformers import get_molformer_embeddings
 
-__version__ = "0.1"
-__author__ = "supergermy"
+# Import models
+from .models.DifferentialAttention import DifferentialAttentionModel
+from .models.PropertyRegressors import (
+    MolecularPropertyRegressor,
+    MolecularPropertyClassifier
+)
 
+# Version information
+__version__ = '0.1.0'
+__author__ = 'Heechan Lee'
+
+# List of public objects that will be exposed when using "from midexam import *"
 __all__ = [
-    # Models
-    'DifferentialAttention',
-    'PropertyPredictor',
-    'PropertyClassifier',
-    'PropertyRegressor',
+    # Data loaders
+    'create_classification_data_loader',
+    'create_regression_data_loader',
     
-    # Dataset loaders
-    'create_data_loaders',
-    'get_chembert2as_embeddings',
-    'get_fingerprints',
-    'get_mol2vec_embeddings',
-    'get_molecular_descriptors',
-    'get_molformers_embeddings'
+    # Molecular representations
+    'generate_fingerprints',
+    'calculate_descriptors',
+    'generate_mol2vec',
+    'get_chembert_embeddings',
+    'get_molformer_embeddings',
+    
+    # Models
+    'DifferentialAttentionModel',
+    'MolecularPropertyRegressor',
+    'MolecularPropertyClassifier',
 ]
