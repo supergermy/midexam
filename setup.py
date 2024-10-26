7caceb4d8d674ec5707c22b6f5dc6f58fc2d8bc4
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:26:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="midexam",
     version="0.1",
-    packages=find_packages(include=['midexam', 'midexam.*']),
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy',
+        'pandas',
+        'scikit-learn',
+    ],
 )
