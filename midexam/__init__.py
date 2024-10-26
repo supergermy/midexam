7290e0a1dfdc1484db554d18d16a5e690897f984
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:41:48 +0900

    Update __init__.py

--- midexam/__init__.py
+++ midexam/__init__.py
@@ -1,8 +1,8 @@
 from .models.PropertyRegressors import PropertyRegressors
 from .datasets.get_fingerprints import get_fingerprints
 from .datasets.get_molecular_descriptors import get_molecular_descriptors
-from .datasets.get_chembert2as import get_chembert_embeddings
-from .datasets.get_molformers import get_molformer_embeddings
+from .datasets.get_chembert2as import get_chembert2as
+from .datasets.get_molformers import get_molformers
 
 __version__ = '0.1'
 
@@ -10,6 +10,6 @@ __all__ = [
     'PropertyRegressors',
     'get_fingerprints',
     'get_molecular_descriptors',
-    'get_chembert_embeddings',
-    'get_molformer_embeddings',
+    'get_chembert2as',
+    'get_molformers',
 ]
