689bf522d7ac04e516dd88425acb3b2a73eeacc9
heechan <102002122+supergermy@users.noreply.github.com>
2024-10-26 15:27:15 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,40 +1,19 @@
-# Import core functionality from datasets
-from .datasets.create_data_loaders import (
-    create_classification_data_loader,
-    create_regression_data_loader
-)
+from .models.PropertyRegressors import MolecularPropertyRegressor
+from .models.DifferentialAttention import DifferentialAttentionModel
 from .datasets.get_fingerprints import generate_fingerprints
 from .datasets.get_molecular_descriptors import calculate_descriptors
 from .datasets.get_mol2vec import generate_mol2vec
 from .datasets.get_chembert2as import get_chembert_embeddings
 from .datasets.get_molformers import get_molformer_embeddings
 
-# Import models
-from .models.DifferentialAttention import DifferentialAttentionModel
-from .models.PropertyRegressors import (
-    MolecularPropertyRegressor,
-    MolecularPropertyClassifier
-)
+__version__ = '0.1'
 
-# Version information
-__version__ = '0.1.0'
-__author__ = 'Heechan Lee'
-
-# List of public objects that will be exposed when using "from midexam import *"
 __all__ = [
-    # Data loaders
-    'create_classification_data_loader',
-    'create_regression_data_loader',
-    
-    # Molecular representations
+    'MolecularPropertyRegressor',
+    'DifferentialAttentionModel',
     'generate_fingerprints',
     'calculate_descriptors',
     'generate_mol2vec',
     'get_chembert_embeddings',
     'get_molformer_embeddings',
-    
-    # Models
-    'DifferentialAttentionModel',
-    'MolecularPropertyRegressor',
-    'MolecularPropertyClassifier',
 ]
